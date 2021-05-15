533bcba248533b4a28f1a04d8b6833129ffa578d
Nawapong Sitaruno <nawaphong.sit@dome.tu.ac.th>
2021-05-16 04:54:01 +0700

    cloud api url

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import vuetify from "./plugins/vuetify";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://localhost:5000/api"
+axios.defaults.baseURL = "http://35.197.141.239:5000/api"
 
 Vue.config.productionTip = false;
 
